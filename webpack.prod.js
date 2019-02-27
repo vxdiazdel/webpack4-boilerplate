45f93d445361f3cb0bd156836d61eb7990f43613
Victor Diaz de Leon <vrdiazdel@gmail.com>
2019-02-27 00:04:37 -0800

    Create initial production config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,1 +1,27 @@
-// TODO
+  const merge = require('webpack-merge');
+  const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+  const common = require('./webpack.common.js');
+
+  module.exports = merge(common, {
+    mode: 'production',
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+            'postcss-loader'
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].[hash].css",
+        chunkFilename: "[id].[hash].css"
+      })
+    ]
+  });
